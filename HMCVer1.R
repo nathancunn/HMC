b0baf99490102175279e41a1e4347134ef2df793
firdey <ho.law@spc.ox.ac.uk>
2015-10-15 11:31:32 +0100

    Version1

--- HMCVer1.R
+++ HMCVer1.R
@@ -4,6 +4,8 @@ HMC1= function(TotalSamples, density, q,M=diag(length(q)),epsilon,L, densitydiff
   d=length(q); #Dimensions
   MassVector=colSums(M); #Mass Of particles
   N=TotalSamples+burnin; #How many samples will be needed
+  Samples=matrix(0,N,2*d);
+  #For allocation of memory
   p=mvnorm(N,rep(0,d),M); 
   #Generate multivariate normal, this is an N by d quantity. I generate all at once here.
   H=runif(N);
@@ -19,12 +21,22 @@ HMC1= function(TotalSamples, density, q,M=diag(length(q)),epsilon,L, densitydiff
     OldUenergy= -log(density(oldq));
     #Energy of target density at old state
     OldKenergy= (oldp/MassVector)%*%(t(oldp));
-    #Energy of momentum
+    #Energy of momentum at Old state
     OldHamiltonian= OldUenergy+OldKenergy;
+    #Old Hamiltonian
     ProposeUenergy= -log(density(qtandepsilon));
     ProposeKenergy=(ptplusepsilon/(2*MassVector))%*%(t(ptplusepsilon));
+    #Propose Energy
     ProposeHamiltonian=ProposeUenergy+ProposeKenergy;
-    NewHamiltonian-
-    U(q)
+    #Propose Hamiltonian
+    if (H[k]<=exp(-OldHamiltonian+ProposeHamiltonian)) {
+      oldp=ptplusepsilon;
+      oldq=qtandepsilon;
+    #Metropolis Hastings, if accepted move to new state
+    }
+    Samples[k,]=rbind(oldp,oldq)
+    #Samples
+    HMC1=Samples[(burnin+1:N),]
+    #forget Intialisation
   }
 }
