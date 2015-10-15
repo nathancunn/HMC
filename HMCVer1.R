21fc31babe0ea71ab9688f6ecb78d9b3e94461c1
Nathan Cunningham <cunningh@tinkerbird.stats.ox.ac.uk>
2015-10-15 17:48:11 +0100

    Corrected leapfrog

--- HMCVer1.R
+++ HMCVer1.R
@@ -1,35 +1,23 @@
 library(MASS)
-HMCVer1= function(TotalSamples, densityA,M, q,epsilon,L, densitydiff, burnin) {
-  oldq=q; #Initialisation of q
-  d=length(q); #Dimensions
-  #MassVector=colSums(M); #Mass Of particles
-  #MassVector=1
+HMCVer1 <- function(TotalSamples, densityA,M, q,epsilon,L, densitydiff, burnin) {
+  oldq <-qtandepsilon <- q #Initialisation of q and qtandepsilon
+  d <- length(q) #Dimensions
   if(length(M)>1){MassVector <- colSums(M)} else MassVector <- M
-  N=TotalSamples+burnin; #How many samples will be needed
-  Samples=matrix(0,N,d);
+  N <- TotalSamples+burnin; #How many samples will be needed
+  Samples <- matrix(0,N,d);
   #For allocation of memory
-  p=mvrnorm(N,rep(0,d),1); 
+  p <- mvrnorm(N,rep(0,d),diag(d)); 
   #Generate multivariate normal, this is an N by d quantity. I generate all at once here.
   for (k in 1:N){
-    oldp=p[k,]; 
-    ptplusepsilonhalf= oldp - epsilon * densitydiff(oldq)/2
-    # Alternate full steps for position and momentum
-    for (i in 1:L)
-    {
-      # Make a full step for the position
-      qtandepsilon = oldq + epsilon * ptplusepsilonhalf
-      # Make a full step for the momentum, except at end of trajectory
-      if (i!=L) ptplusepsilonhalf = ptplusepsilonhalf - epsilon * densitydiff(qtandepsilon) }
-    # Make a half step for momentum at the end.
-    ptplusepsilon = ptplusepsilonhalf - epsilon * densitydiff(qtandepsilon)/2
+    oldp <-ptplusepsilon <- p[k,] 
     #Take one momentum vector.
-  #  for (t in 1:L){
+    for (t in 1:L){
       #LeapFrog L times with step size epsilon
-   #   ptplusepsilonhalf= oldp- (epsilon/2)*densitydiff(oldq);
-    #  qtandepsilon =  oldq + ((epsilon/MassVector)*ptplusepsilonhalf);
+      ptplusepsilonhalf= ptplusepsilon - (epsilon/2)*densitydiff(qtandepsilon)
+      qtandepsilon =  qtandepsilon + ((epsilon/MassVector)*ptplusepsilonhalf)
       #Assume independent in p vector hence use column sums
-    #  ptplusepsilon= ptplusepsilonhalf -(epsilon/2)*densitydiff(qtandepsilon);
-    #}
+      ptplusepsilon= ptplusepsilonhalf - (epsilon/2)*densitydiff(qtandepsilon)
+    }
     OldUenergy= -log(densityA(oldq));
     #Energy of target densityA at old state
     OldKenergy= sum((oldp^2/MassVector))/2;
@@ -56,32 +44,11 @@ HMCVer1= function(TotalSamples, densityA,M, q,epsilon,L, densitydiff, burnin) {
 }
 
 
-JJ <- HMCVer1(TotalSamples = 10000,densityA = dnorm,q = 0,epsilon = 0.2,L = 20,densitydiff = function(x) x,burnin = 0)
-TotalSamples = 100
-density = dmvnorm
-q=c(0,0)
-M=diag(length(q))
-epsilon=0.2
-L=20
-densitydiff = function(x) x
-burnin=0
+JJ <- HMCVer1(TotalSamples = 10000,densityA = dnorm,M=1,q = 0,epsilon = 0.05,L = 20,densitydiff = function(x) x,burnin = 0)
+hist(JJ,freq=F)
+lines(seq(-4,4,0.01),dnorm(seq(-4,4,0.01)))
 
-
-
-plot(JJ[,1])
-lag.plot(JJ[,1])
-lag(JJ[,1],1)
-JJ[,1]
-hist(JJ[,1])
-
-plot(JJ[,1],type="b")
-
-
-
-JJ <- HMCVer1(TotalSamples = 100,densityA = dmvnorm,q = c(0,0),M=diag(length(q)),epsilon = 0.2,L = 20,densitydiff = function(x) x,burnin = 0)
-
-
-
+# Multivariate example - needs work
 JJ <- HMCVer1(TotalSamples = 1000,densityA = function(l) dmvnorm(l,c(0,0),matrix(c(1,0.7,0.7,1),2,2)),q = c(0,0),M=diag(length(q)),epsilon = 0.05,L = 60,densitydiff = function(x) t(t(x)%*%solve(matrix(c(1,0.7,0.7,1),2,2))), burnin = 0)
 
 
