50137d99f51bdebdc12b41976d027581e5315aca
Nathan Cunningham <cunningh@tinkerbird.stats.ox.ac.uk>
2015-10-16 12:57:57 +0100

    working letters file

--- letters.R
+++ letters.R
@@ -47,10 +47,11 @@ k <- 1
 
 library(mixtools)
 
-seqn <- floor(seq(1,nrow(m),length=64))
-compressed <- matrix(0,64,64)
-for(i in 1:63){
-  for(j in 1:63){
+maxdim <- 32
+seqn <- floor(seq(1,nrow(m),length=maxdim))
+compressed <- matrix(0,maxdim,maxdim)
+for(i in 1:(maxdim-1)){
+  for(j in 1:(maxdim-1)){
  compressed[i,j] <- mean(m[seqn[i]:seqn[i+1],seqn[j]:seqn[j+1]])  
   }
 }
@@ -65,8 +66,54 @@ for(i in 1:nrow(compressed)) {
     k <- k+1
   }
 }
+plot(coords[,1],coords[,2],col=coords[,3],asp=1)
+
+out <- mvnormalmixEM(coords[coords[,3]==1,1:2],k=2)
+#Creating each of the letters as images
+setwd("Letters/")
+all.letters <- c(letters,toupper(letters),"?","!")
+for(i in 1:length(all.letters)){
+jpeg(filename = paste(all.letters[i],".jpg",sep=""))
+plot(0,0,pch=all.letters[i],xaxt="n",yaxt="n",ann=F, bty="n",cex=35)
+dev.off()
+}
 
 
-out <- mvnormalmixEM(coords[coords[,3]==1,1:2],k=4)
+out <- list()
+files <- list.files()
+maxdim <- 32 #allows for a 32*32 grid representing the letters
+seqn <- floor(seq(1,nrow(m),length=maxdim))
 
-
+for(i in 1:length(files)) {
+  # Read in the image and convert it into a matrix
+  img <- readJPEG(files[i])
+  m <- as.raster(img, max = 255)
+  m[m=="#000000"] <- 1
+  m[m=="#010101"] <- 0
+  # Convert the matrix to numeric
+  m <- matrix(as.numeric(unlist(m)),nrow=nrow(m))
+  # Compressing the matrix to save on computation time
+  compressed <- matrix(0,maxdim,maxdim)
+  for(j in 1:(maxdim-1)){
+    for(k in 1:(maxdim-1)){
+      compressed[j,k] <- mean(m[seqn[j]:seqn[j+1],seqn[k]:seqn[k+1]])  
+    }
+  }
+  # Rounding so we only have 1's and 0's
+  compressed <- round(compressed)
+  # Converting the matrix to a co-ordinates matrix
+  coords <- matrix(0,nrow=length(compressed),ncol=3)
+  l <- 1
+  for(j in 1:nrow(compressed)) {
+    for(k in 1:ncol(compressed)) {
+      coords[l,] <- c(j,32-k,compressed[j,k])
+      l <- l+1
+    }
+  }
+  gauss.mix <- mvnormalmixEM(coords[coords[,3]==1,1:2],k=4)
+  out[[i]] <- c(letter = all.letters[i],
+                lambda = gauss.mix$lambda,
+                mu = gauss.mix$mu,
+                sigma = gauss.mix$sigma)
+}
+plot(coords[,1],coords[,2],col=coords[,3],asp=1)
