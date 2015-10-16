6aca2b097fd0b38e4700f9c199bd9f32d065a3f2
firdey <ho.law@spc.ox.ac.uk>
2015-10-16 13:54:15 +0100

    Working MH multi Normal

--- MHDim.R
+++ MHDim.R
@@ -1,17 +1,21 @@
-MetropolisHastingC = function(SamplesN, densityInt, sigma, initial) {
+MetropolisHastingC = function(TamplesN, densityInt, sigma, initial,thin) {
+  SamplesN=thin*TamplesN;
   OldPos=initial;
   OutputSamples= matrix(0,SamplesN,length(initial));
   for (i in 1:SamplesN)
 {
-    Proposal= rmvnorm(1,OldPos,as.matrix(sigma));
+    Proposal<- rmvnorm(1,as.vector(OldPos),sigma);
     ProposedValue=densityInt(as.vector(Proposal));
-    OldValue=densityInt(OldPos);
+    OldValue=densityInt(as.vector(OldPos));
     if (runif(1)<= (ProposedValue/OldValue))
 {
       OldPos=Proposal;
     }
 OutputSamples[i,]=OldPos
   }
-return(OutputSamples)
+FinalOutputSamples=OutputSamples[seq(1,dim(OutputSamples)[1],thin),]
+return(FinalOutputSamples)
 }
-MetropolisHastingC(1000,function(j){dnorm(j,0,1)},1,0)
+MetropolisHastingC(1000,function(j){dnorm(j,0,1)},1,0,10)
+
+MetropolisHastingC(1000,function(j){dmvnorm(j,c(0,0),matrix(c(1,0.9,0.9,1),2,2))},diag(2),c(0,0),10)
