cc05aef405f531dbec15b60ed2448001a9a80871
nathancunn <nathancunn@gmail.com>
2015-10-19 15:45:46 +0100

    rstan with letter sampling

--- rstan.R
+++ rstan.R
@@ -1,31 +1,9 @@
-dotR <- file.path(Sys.getenv("HOME"), ".R")
-if (!file.exists(dotR)) dir.create(dotR)
-M <- file.path(dotR, "Makevars")
-if (!file.exists(M)) file.create(M)
-cat("\nCXXFLAGS=-O3", file = M, sep = "\n", append = TRUE)
-
-install.packages("rstan", dependencies = TRUE)
-
-
-library(rstan)
-rstan_options(auto_write = TRUE)
-options(mc.cores = parallel::detectCores())
-
-
-
-
-
-K <- 2 # number of mixing groups
-lambda <- c(0.3,1) # mixing proportions
-d <- 2 # number of dimensions
-mu <- matrix(c(0,10,20,30), nrow = 2, byrow = T) # mean vectors row by row
-Sigmas <- matrix(data = c(1,0.9,0.9,1,1,-0.9,-0.9,1), ncol = 2, byrow = T) # covariance matrices row by row
 m <- stan_model(model_code = 'data {
                 int<lower=1> K;
                 int<lower=1> d;
                 row_vector[K] lambda;
                 matrix[K,d] mu;
-                matrix[d*K,K] Sigmas;
+                matrix[d*K,d] Sigmas;
                 }
                 parameters {
                 matrix[K,d] x;
@@ -51,6 +29,21 @@ m <- stan_model(model_code = 'data {
                 
                 U ~ uniform(0,1);
                 }')
-f <- sampling(m, iter = 10000,chains=1)
-hist(f@sim$samples[[1]]$`z[1]`)
-plot(f@sim$samples[[1]]$`z[1]`,f@sim$samples[[1]]$`z[2]`)
+
+letter <- A[[6]]
+K <- (length(letter)-1)/3
+lambda <- cumsum(letter[2:(1+K)]) # mixing proportions
+d <- 2 # number of dimensions
+mu <- letter[[2+K]] # mean vectors row by row
+for(i in 2:K) { 
+  mu <- rbind(mu,letter[[1+K+i]])
+}
+Sigmas <- letter[[2+2*K]]
+for(i in 2:K) {
+  Sigmas <- rbind(Sigmas,letter[[1+2*K+i]])
+}
+
+
+f <- sampling(m, iter = 5000,chains=1)
+hist(f@sim$samples[[1]]$`z[3]`)
+plot(f@sim$samples[[1]]$`z[1]`,f@sim$samples[[1]]$`z[2]`,asp = 1)
