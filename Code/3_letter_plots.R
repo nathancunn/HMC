7c73ed1a01761542b1d4a25fee8fb78ff6f3752b
Nathan Cunningham <cunningh@tinkerbird.stats.ox.ac.uk>
2015-10-21 17:10:20 +0100

    Code

--- Code/3_letter_plots.R
+++ Code/3_letter_plots.R
@@ -26,16 +26,20 @@ WordForm <- function(word) {
 
 
 
-
+# The function to generate a dataset of random points from a distribution
+# whose probability density spells a given word
 WordPrint <- function(word,samples) {
   letters.key <- WordForm(word)
+  # An array of the initial values for the Hamiltonian Monte Carlo
   last.h = array(0,c(nchar(word),7,2))
-  out <- matrix(0,nrow=samples,ncol=2)
+  # A matrix containing the simulated sample
+  out <- matrix(0,nrow=samples,ncol=5)
+  # The number of letters
   n <- length(letters.key)
   # Give each letter equal weight
   eq_probs <- seq(1/n,1,length=n)
-  # Randomly go to a single letter
   for (i in 1:samples) {
+  # Randomly go to a single letter
   letter <- findInterval(runif(1),eq_probs)+1
   j <- letters.key[letter]
   # Displace letters so they can be seen side-by-side
@@ -53,7 +57,8 @@ WordPrint <- function(word,samples) {
   if (last.h[letter,group,1]==0 & last.h[letter,group,2]==0) {
     last.h[letter,group,]=as.vector(mean.group)
   }
-  out[i,] <- HMC(total.samples = 1,
+  # Simulate a single datum from the selected Gaussian mixture
+  out[i,1:2] <- HMC(total.samples = 1,
              q.density = function(l) dmvnorm(l,as.vector(mean.group),var.group), 
              q = as.vector(last.h[letter,group,]), M=diag(2),
              epsilon = 0.05, 
@@ -61,12 +66,15 @@ WordPrint <- function(word,samples) {
              diff.density = function(x) {
                solve(var.group)%*%(as.matrix(x)-as.matrix(mean.group))}, 
              burnin = 0, output = 0)
-  last.h[letter,group,] <- out[i,]
+  # Store the value to be used as the initial value when this mixture is used again
+  last.h[letter,group,] <- out[i,1:2]
+  # Store a unique colour for each mixture to see where the mixtures are
+  out[i,3:5] <- c(letter/n,group/6,0.2)
   }
   out
 }
 
-plot(WordPrint("OxWaSP",1000))
-
+oxwasp <- WordPrint("OxWaSP",2500)
+plot(oxwasp[,1],oxwasp[,2],col=rgb(oxwasp[,3],oxwasp[,4],oxwasp[,5]))
 
 
