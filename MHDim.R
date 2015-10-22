9936fa4ca4bb25193090655aba344de8b89f862f
firdey <ho.law@spc.ox.ac.uk>
2015-10-22 11:22:36 +0100

    New

--- MHDim.R
+++ MHDim.R
@@ -1,11 +1,11 @@
 MetropolisHastingC = function(TamplesN, densityInt, sigma, initial,thin) {
-  now=Sys.time()
+ # now=Sys.time()
   #Multivariate Normal with Samples, integration, (sigma in this case for 2-dim),initial conditions, thinning (1 if no thinning)
   SamplesN=thin*TamplesN;
   #Works out how much thinning
   OldPos=initial;
   OutputSamples= matrix(0,SamplesN,length(initial));
-  pb <- txtProgressBar(min = 0, max = SamplesN, style = 3);
+ # pb <- txtProgressBar(min = 0, max = SamplesN, style = 3);
   #Allocation
   Dimensions=length(initial);
   for (i in 1:SamplesN)
@@ -23,7 +23,7 @@ MetropolisHastingC = function(TamplesN, densityInt, sigma, initial,thin) {
       OldPos=Proposal;
     }
 OutputSamples[i,]=OldPos
-setTxtProgressBar(pb, i)
+#setTxtProgressBar(pb, i)
   }
 if (TamplesN==1)
 {
@@ -36,10 +36,10 @@ FinalOutputSamples=OutputSamples[seq(1,dim(OutputSamples)[1],thin),] }
 #plot(FinalOutputSamples[,1],FinalOutputSamples[,2],type='l',col=2,xlab="X",ylab="Y",xlim=c(-2,2),ylim=c(-2,2))
 #points(FinalOutputSamples[,1],FinalOutputSamples[,2],pch=4)
 #cat("rejectionRate=",1-(Size[1]/SamplesN))
-after=Sys.time()-now;
-print(after)
+#after=Sys.time()-now;
+#print(after)
 return(FinalOutputSamples)
-close(pb)
+#close(pb)
 }
 MetropolisHastingC(1000,function(j){dnorm(j,0,1)},1,0,10)
 
@@ -67,7 +67,7 @@ for(i in 1:26) {
   }
 }
 
-WordprintMH <- function(word,lastValueH) {
+WordprintMH <- function(word,lastValueH,thin) {
   #Set up the word 
   letters <- wordform(word)
   n <- length(letters)
@@ -85,13 +85,14 @@ WordprintMH <- function(word,lastValueH) {
   if (lastValueH[letter,group,1]==0 & lastValueH[letter,group,2]==0)
   {lastValueH[letter,group,]=as.vector(mean.group)}
   #If no starting value, use the mean
-  FinalOutput=MetropolisHastingC(1, function(l) {dmvnorm(l,as.vector(mean.group)-c(3,3),var.group)},sqrt(sqrt(max(as.vector(var.group)))), as.vector(lastValueH[letter,group,]),1) 
+  FinalOutput=MetropolisHastingC(1, function(l) {dmvnorm(l,as.vector(mean.group)-c(3,3),var.group)},sqrt(sqrt(max(as.vector(var.group)))), as.vector(lastValueH[letter,group,]),thin) 
   #Use Metropolis Hasting algorithm, use the last position.
   #points(rmvnorm(1,mu = mean.group, sigma = var.group),cex=0.75,col=cols[letter]) 
   (list(FinalOutput,letter,group)) 
 }
-WordOutputAllMH= function(Word,SampleSize){
+WordOutputAllMH= function(Word,SampleSize,thin=1){
   #Takes in a string, sample size.
+  now=Sys.time()
   plot.new()
   FinalOutput=matrix(0,SampleSize,2);
   ColorsMemory=matrix(0,SampleSize,2)
@@ -99,7 +100,7 @@ WordOutputAllMH= function(Word,SampleSize){
   LastValueG <- array(0,c(length.of.string,6,2));
   #Allocation
   for (i in 1:SampleSize) {
-    OutputOnce<-WordprintMH(Word,LastValueG)
+    OutputOnce<-WordprintMH(Word,LastValueG,thin)
     #Metropolis Hasting using last update
     FinalOutput[i,]=OutputOnce[[1]]
     #Final output
@@ -119,7 +120,7 @@ WordOutputAllMH= function(Word,SampleSize){
   FinalOutputMaxY=max(FinalOutput[,2])
   #Computes Limit
   #quartz()
-  plot(FinalOutput[1,],cex=0.75,xlim=c(FinalOutputMinX-2,FinalOutputMaxX+2),ylim=c(FinalOutputMinY-2,FinalOutputMaxY+2),col=rgb(ColorsMemory[1,1]/length.of.string,ColorsMemory[1,2]/6,0.2))
+  plot(FinalOutput[1,],cex=0.75,xlim=c(FinalOutputMinX-2,FinalOutputMaxX+2),ylim=c(FinalOutputMinY-2,FinalOutputMaxY+2),col=rgb(ColorsMemory[1,1]/length.of.string,ColorsMemory[1,2]/6,0.2),xlab="",ylab="",main="MH")
   #Plot first point
   #Sys.sleep(1)
   for (i in 2:SampleSize){
@@ -127,6 +128,8 @@ WordOutputAllMH= function(Word,SampleSize){
   #Plot mixture and letter different colours.   
   #Sys.sleep(0.01)
   Size=dim(unique(FinalOutput))
+  after=Sys.time()-now
+  print(after)
   cat("Total Samples after Burn in:",SampleSize,"\nMean Of Samples=",apply(FinalOutput,2,mean),"\nCovariance=",cov(FinalOutput), "\nRejectionRate=",1-(Size[1]/SampleSize))
   return(FinalOutput)
 }
